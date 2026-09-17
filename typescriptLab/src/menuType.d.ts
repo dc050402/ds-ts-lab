67f40380f64a7a04e54960b1783663892c68f454
dc425 <dylconway652@gmail.com>
2026-09-17 11:55:28 +0100

    Added literal union type and fixed course: string to course: Course

--- typescriptLab/src/menuType.d.ts
+++ typescriptLab/src/menuType.d.ts
@@ -1,7 +1,9 @@
+type Course = "starter" | "main" | "dessert";
+
 export interface MenuItems {
     id: number;
     name: string;
-    course: string;
+    course: Course;
     price: number;
     discountPercent? : number;
     availableFrom: Date;
