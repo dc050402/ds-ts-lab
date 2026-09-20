234ecf4f8dccb810e0445338b3e1e85c5b12ae21
dc425 <dylconway652@gmail.com>
2026-09-20 18:42:11 +0100

    Made first match function generic

--- typescriptLab/src/cafe.ts
+++ typescriptLab/src/cafe.ts
@@ -114,7 +114,7 @@ function cheapest(items: MenuItem[], max?: number): MenuItem[] {
 
 // TS: This function works on any array, not just menu items. Make it
 //     *generic*: <T>(data: T[], criteria: (d: T) => boolean) => T | undefined.
-function firstMatch(data, criteria) {
+function firstMatch<T>(data: T[], criteria: (d: T) => boolean): T | undefined {
   return data.find(criteria);
 }
 
